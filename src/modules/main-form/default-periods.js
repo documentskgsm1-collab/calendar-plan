749ca95fb2d9d8fa9339fc74a2451e5a0ad6acf5
documentskgsm1-collab <documentskgsm1@gmail.com>
2026-08-25 22:00:44 +0300

    Update default-periods.js

--- src/modules/main-form/default-periods.js
+++ src/modules/main-form/default-periods.js
@@ -3,12 +3,12 @@ import {firstSemesterYear, secondSemesterYear} from '../holidays/current-period'
 const defaultPeriods = () => {
   document.getElementById("btnFirstSemester").addEventListener("click", () => {
     document.getElementById("start-date").value = `${firstSemesterYear}-09-01`
-    document.getElementById("end-date").value = `${firstSemesterYear}-12-31`
+    document.getElementById("end-date").value = `${firstSemesterYear}-12-23`
   })
 
   document.getElementById("btnSecondSemester").addEventListener("click", () => {
-    document.getElementById("start-date").value = `${secondSemesterYear}-01-01`
-    document.getElementById("end-date").value = `${secondSemesterYear}-05-31`
+    document.getElementById("start-date").value = `${secondSemesterYear}-01-11`
+    document.getElementById("end-date").value = `${secondSemesterYear}-05-28`
   })
 }
 
